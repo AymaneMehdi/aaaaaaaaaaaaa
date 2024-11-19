51b7d10f83b540590deb98c5f7bd13b28b6d2f04
Aymane Mehdi <unlocked.dark.web@gmail.com>
2024-11-19 17:45:33 +0100

    GO

--- app/components/admin/AdminNav.tsx
+++ app/components/admin/AdminNav.tsx
@@ -18,11 +18,11 @@ const AdminNav = () => {
     <div className=" w-full shadow-sm top-20 border-b-[1px] pt-4 bg-[#ffffff] ">
       <Container>
         <div className="flex flex-row items-center justify-between md:justify-center gap-8 md:gap-12 overflow-x-auto flex-nowrap bg-[#ffffff] ">
-          <Link href="/admin">
+          <Link href="/add-products">
             <AdminNavItem
               label="Summary"
               icon={MdDashboard}
-              selected={pathname === "/admin"}
+              selected={pathname === "/add-products"}
             />
           </Link>
           <Link href="/admin/add-products">
